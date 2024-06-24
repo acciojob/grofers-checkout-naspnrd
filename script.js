27e7ea26f081a0ed3a060fe47073236c0dc2f07a
Neeraj Chaudhary <36655557+naspnrd@users.noreply.github.com>
2024-06-24 22:40:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 const getSumBtn = document.createElement("button");
-getSumBtn.append("Get Total Price");
+getSumBtn.append("Get Total Price"); 
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-  
+  getSumBtn.disabled = true;
+  const prices = document.querySelectorAll(".price"); 
+	let sum = 0;
+	prices.forEach((price) => {
+		const value = parseInt(price.textContent);
+		if(!isNaN(value)){
+			sum += value;
+		}
+	})
+	const totalPriceRow = document.createElement("tr");
+	totalPriceRow.id = "ans";
+	const totalPriceData = document.createElement("td");
+	const totalPriceAns = document.createElement("td");
+	totalPriceRow.appendChild(totalPriceData);
+	totalPriceRow.appendChild(totalPriceAns);
+	totalPriceData.append('Total Price (in Rs): ');
+	totalPriceAns.append(sum)
+
+	const table = document.querySelector("tbody");
+	table.appendChild(totalPriceRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
